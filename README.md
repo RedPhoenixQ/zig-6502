fbaaa38ca9cfe968a4c57d22c8f57ad117638955
RedPhoenixQ <steve.dark.03@gmail.com>
2024-10-26 16:07:07 +0200

    Manually aasseble test binary for newer tests

---
+++ README.md
@@ -0,0 +1,5 @@
+# Assemble test file
+
+```console
+./assembler/as65.exe ./src/tests/6502_functional_test.a65 -x1 -n -l./src/tests/6502_functional_test.lst -o./src/tests/6502_functional_test.bin -w -h0 -m -v
+```
--- src/main.zig
+++ src/main.zig
@@ -19,7 +19,7 @@ pub fn main() !void {
 test "functional test" {
     const test_binary = @embedFile("./tests/6502_functional_test.bin");
     var mem = try std.testing.allocator.create([0x10000]u8);
-    @memcpy(mem[0..], test_binary[0..]);
+    @memcpy(mem[0xa..], test_binary[0..]);
     var cpu = CPU.new(mem.*);
     cpu.registers.program_counter = 0x0400 - 1;
     cpu.printRegisters();
