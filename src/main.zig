4c414cec137a343170077c3ee8e711dd87d38dc2
RedPhoenixQ <steve.dark.03@gmail.com>
2024-10-27 17:26:10 +0100

    Fix main CPU init

--- src/main.zig
+++ src/main.zig
@@ -3,15 +3,15 @@ const std = @import("std");
 const CPU = @import("./6502.zig");
 
 pub fn main() !void {
-    var mem = std.mem.zeroes(CPU.Memory);
-    mem[1] = @intFromEnum(CPU.Op.LDA_IMM);
-    mem[2] = 0x42;
-    mem[3] = @intFromEnum(CPU.Op.LDA_IMM);
-    mem[4] = 0xAA;
-    mem[CPU.POWER_ON_RESET_ADDRESS] = 0x03 - 1;
+    var cpu: CPU = .{};
 
-    var cpu = CPU.new(mem);
-    cpu.step();
+    cpu.memory[1] = @intFromEnum(CPU.Op.LDA_IMM);
+    cpu.memory[2] = 0x42;
+    cpu.memory[3] = @intFromEnum(CPU.Op.LDA_IMM);
+    cpu.memory[4] = 0xAA;
+    cpu.memory[CPU.POWER_ON_RESET_ADDRESS] = 0x03 - 1;
+
+    _ = cpu.step();
 
     std.debug.print("{}{}", .{ cpu.registers, cpu.flags });
 }
